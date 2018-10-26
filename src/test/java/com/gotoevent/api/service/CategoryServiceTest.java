3f786e7a77921e7a418c0a2ad8edb7dee4f663e6
dunkansdk <esqueje18@gmail.com>
2018-10-26 00:02:41 -0300

    mockito test (entity: category)

---
+++ src/test/java/com/gotoevent/api/service/CategoryServiceTest.java
@@ -0,0 +1,46 @@
+package com.gotoevent.api.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.gotoevent.api.entity.Category;
+import com.gotoevent.api.repository.CategoryRepository;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class CategoryServiceTest {
+	
+	@Mock
+	private CategoryRepository categoryRepository;
+	
+	@InjectMocks
+	private CategoryService categoryService;
+	
+	Category category = new Category(1, "CategoriaTest");
+	
+	@Test
+    public void addTest()throws Exception {
+        when(this.categoryRepository.save(this.category)).thenReturn(this.category);
+        
+        Category testCategory = this.categoryService.newObject(this.category);
+        assertEquals(1,testCategory.getId());
+        assertEquals("CategoriaTest", testCategory.getName());
+
+    }
+	
+	@Test
+    public void getByIdTest()throws Exception {
+        when(this.categoryRepository.findById(this.category.getId())).thenReturn(java.util.Optional.ofNullable(this.category));
+        
+        Category testCategory =this.categoryService.getById(this.category.getId());        
+        assertEquals(1, testCategory.getId());
+        assertEquals("CategoriaTest", testCategory.getName());
+ 
+    }	
+
+}
